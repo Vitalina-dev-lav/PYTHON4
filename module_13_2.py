4401bff7c4d9617765d4d5524f4c2e8741de07f9
Vitalina-dev-lav <vitalinabatrakova@gmail.com>
2024-12-25 16:17:13 +0300

    H

--- module_13_2.py
+++ module_13_2.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7670310044:AAGRpEMo3q5EHEBpybebnW5XjqyRpiYbKtc"
+api = "!!!!!!!!"
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
